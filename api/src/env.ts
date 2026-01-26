a615ff34f6a13a3ddc9a33b9df5466fe5b13a02e
khiem <khiemnguyen.hye@gmail.com>
2026-01-27 00:23:38 +0700

    Add upload route

--- api/src/env.ts
+++ api/src/env.ts
@@ -10,5 +10,9 @@ export const env = {
   PORT: Number(required('PORT')),
   ADMIN_PASSWORD: required('ADMIN_PASSWORD'),
   ADMIN_USERNAME: required('ADMIN_USERNAME'),
-  JWT_SECRET: required('JWT_SECRET')
+  JWT_SECRET: required('JWT_SECRET'),
+  S3_ENDPOINT: required('S3_ENDPOINT'),
+  S3_BUCKET_NAME: required('S3_BUCKET_NAME'),
+  S3_ACCESS_KEY_ID: required('S3_ACCESS_KEY_ID'),
+  S3_SECRET_ACCESS_KEY: required('S3_SECRET_ACCESS_KEY'),
 }
