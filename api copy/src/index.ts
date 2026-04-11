1e4a95113dccaec4bbc351d6f67c74d345d86756
Bush <tranphuonglinh081706@gmail.com>
2026-04-11 13:31:59 +0700

    Semantics

---
+++ api copy/src/index.ts
@@ -0,0 +1,24 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { env } from './env.ts'
+import { healthRouter } from './features/health/health.route.ts'
+import { authRouter } from './features/auth/auth.route.ts'
+import { cors } from 'hono/cors'
+import { fileRouter } from './features/file/files.route.ts'
+
+const app = new Hono()
+
+app.use('*', cors({
+  origin: ['http://localhost:3000', 'https://anyudock.cloud']
+}))
+
+app.route('/health', healthRouter)
+app.route('/auth', authRouter)
+app.route('/file', fileRouter)
+
+serve({
+  fetch: app.fetch,
+  port: env.PORT
+}, (info) => {
+  console.log(`Server is running on port ${info.port}`)
+})
---
+++ api copy/src/middlewares/auth.middleware.ts
@@ -0,0 +1,20 @@
+import { verify } from "hono/jwt";
+import { createMiddleware } from "hono/factory";
+import { env } from "../env.ts";
+
+export const authMiddleware = createMiddleware(async (c, next) => {
+  const header = c.req.header('Authorization')
+
+  if (!header?.startsWith('Bearer ')) {
+    return c.json({ error: 'Unauthorized' }, 401)
+  }
+
+  const token = header.slice(7)
+
+  try {
+    await verify(token, env.JWT_SECRET, { alg: 'HS256' })
+    await next()
+  } catch (e) {
+    return c.json({ error: 'Invalid token' }, 401)
+  }
+})
